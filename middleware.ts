7c09ce9fcc6395fccdc473715fd016d3619dc16c
walid4mp <you@example.com>
2026-04-15 00:24:23 +0100

    fix middleware error

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
-import { updateSession } from '@/lib/supabase/proxy'
-import { type NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-}
+  matcher: []
+};
